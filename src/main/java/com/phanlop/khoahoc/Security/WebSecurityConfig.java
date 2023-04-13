cfc0cf18775ed6e3e4d3d3ae59bf13fb113de52c
huykhaduy <huykhaduy@gmail.com>
2023-04-13 09:34:21 +0700

    add drawio

--- src/main/java/com/phanlop/khoahoc/Security/WebSecurityConfig.java
+++ src/main/java/com/phanlop/khoahoc/Security/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.phanlop.khoahoc.Security;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +8,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
