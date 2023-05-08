e0b231dddbaba5583a4097f53d2af5591d46baaf
Quan16022003 <89841066+Quan16022003@users.noreply.github.com>
2023-05-08 18:18:40 +0700

    add course_intro

--- src/main/java/com/phanlop/khoahoc/Controller/HomeController.java
+++ src/main/java/com/phanlop/khoahoc/Controller/HomeController.java
@@ -1,16 +1,15 @@
 package com.phanlop.khoahoc.Controller;
 
 import com.phanlop.khoahoc.Config.CustomUserDetails;
-import com.phanlop.khoahoc.DTO.CourseDTO;
 import com.phanlop.khoahoc.Entity.Course;
 import com.phanlop.khoahoc.Entity.User;
+import com.phanlop.khoahoc.Repository.CourseRepository;
 import com.phanlop.khoahoc.Repository.DepartmentRepository;
 import com.phanlop.khoahoc.Service.CourseServices;
 import com.phanlop.khoahoc.Service.UserServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,12 +17,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     private final DepartmentRepository departmentRepository;
+    private final CourseRepository courseRepository;
     private final CourseServices courseService;
     private final UserServices userServices;
 
@@ -48,7 +49,11 @@ public class HomeController {
 
     @PreAuthorize("hasRole('ROLE_STUDENT')")
     @GetMapping("/detail/{courseId}")
-    public String getDetail(String courseId) {
+    public String getDetail(@PathVariable String courseId, Model model) {
+        Course myCourse = courseRepository.findById(UUID.fromString(courseId)).get();
+        model.addAttribute("course", myCourse);
+        model.addAttribute("chapters", myCourse.getListChapters().toArray());
+        System.out.println(myCourse.toString());
         return "course_intro";
     }
 }
